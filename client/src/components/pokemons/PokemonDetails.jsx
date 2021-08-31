ee946377885ca93cf48acfc707a9be86a3d5a13c
LGLT <leonardo.lopez.jobs@gmail.com>
2021-08-31 12:09:38 -0500

    Added: Styles in pokemonDetails

--- client/src/components/pokemons/PokemonDetails.jsx
+++ client/src/components/pokemons/PokemonDetails.jsx
@@ -21,24 +21,38 @@ export default function PokemonDetails(){
                 <div className={styles.title}>
                     <h2>{capitalize(pokemonData[0][0])}</h2>
                 </div>
-                <img src={pokemonData[0][2].data[0].sprites.front_default} alt="img" />
-                <div> 
+                <img src={pokemonData[0][2].data[0].sprites.front_default} alt="img" className={styles.img} />
+                <div className={styles.divStats}> 
                     <h3>Stats</h3>
-                    <div>
-                    <h4>Pokemon ID:</h4>
-                    <p>{pokemonData[0][2].data[0].id}</p>
-                    <h4>Health Points (HP):</h4>
-                    <p>{pokemonData[0][2].data[0].stats[0].base_stat}</p>
-                    <h4>Attack:</h4>
-                    <p>{pokemonData[0][2].data[0].stats[1].base_stat}</p>
-                    <h4>Defense:</h4>
-                    <p>{pokemonData[0][2].data[0].stats[2].base_stat}</p>
-                    <h4>Speed:</h4>
-                    <p>{pokemonData[0][2].data[0].stats[5].base_stat}</p>
-                    <h4>Height:</h4>
-                    <p>{pokemonData[0][2].data[0].height}</p>
-                    <h4>weight:</h4>
-                    <p>{pokemonData[0][2].data[0].weight}</p>
+                    <div className={styles.secondDivStats}>
+                        <div className={styles.stat}>
+                            <h4 style={{fontWeight: "400"}}>Pokemon ID:</h4>
+                            <p>{pokemonData[0][2].data[0].id}</p>
+                        </div>
+                        <div className={styles.stat}>
+                            <h4 style={{fontWeight: "400"}}>Health Points (HP):</h4>
+                            <p>{pokemonData[0][2].data[0].stats[0].base_stat}</p>
+                        </div>
+                        <div className={styles.stat}>
+                            <h4 style={{fontWeight: "400"}}>Attack:</h4>
+                            <p>{pokemonData[0][2].data[0].stats[1].base_stat}</p>
+                        </div>
+                        <div className={styles.stat}>
+                            <h4 style={{fontWeight: "400"}}>Defense:</h4>
+                            <p>{pokemonData[0][2].data[0].stats[2].base_stat}</p>
+                        </div>
+                        <div className={styles.stat}>
+                            <h4 style={{fontWeight: "400"}}>Speed:</h4>
+                            <p>{pokemonData[0][2].data[0].stats[5].base_stat}</p>
+                        </div>
+                        <div className={styles.stat}>
+                            <h4 style={{fontWeight: "400"}}>Height:</h4>
+                            <p>{pokemonData[0][2].data[0].height}</p>
+                        </div>
+                        <div className={styles.stat}>
+                            <h4 style={{fontWeight: "400"}}>Weight:</h4>
+                            <p>{pokemonData[0][2].data[0].weight}</p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -52,20 +66,34 @@ export default function PokemonDetails(){
                 <div> 
                     <h3>Stats</h3>
                     <div>
-                    <h4>Pokemon ID:</h4>
-                    <p>{pokemonData[0].id}</p>
-                    <h4>Health Points (HP):</h4>
-                    <p>{pokemonData[0].hp}</p>
-                    <h4>Attack:</h4>
-                    <p>{pokemonData[0].attack}</p>
-                    <h4>Defense:</h4>
-                    <p>{pokemonData[0].defense}</p>
-                    <h4>Speed:</h4>
-                    <p>{pokemonData[0].speed}</p>
-                    <h4>Height:</h4>
-                    <p>{pokemonData[0].height}</p>
-                    <h4>weight:</h4>
-                    <p>{pokemonData[0].weight}</p>
+                        <div>
+                            <h4>Pokemon ID:</h4>
+                            <p>{pokemonData[0].id}</p>
+                        </div>
+                        <div>
+                            <h4>Health Points (HP):</h4>
+                            <p>{pokemonData[0].hp}</p>
+                        </div>
+                        <div>
+                            <h4>Attack:</h4>
+                            <p>{pokemonData[0].attack}</p>
+                        </div>
+                        <div>
+                            <h4>Defense:</h4>
+                            <p>{pokemonData[0].defense}</p>
+                        </div>
+                        <div>
+                            <h4>Speed:</h4>
+                            <p>{pokemonData[0].speed}</p>
+                        </div>
+                        <div>
+                            <h4>Height:</h4>
+                            <p>{pokemonData[0].height}</p>
+                        </div>
+                        <div>
+                            <h4>weight:</h4>
+                            <p>{pokemonData[0].weight}</p>
+                        </div>
                     </div>
                 </div>
             </div>
